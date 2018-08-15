5e0d30a2edda9dc3b93339aec714481ff665e870
Filip Kuszenin <filipkuszenin@gmail.com>
2018-08-15 14:08:55 +0100

    Initial commit. music_collection db created. Artists and albums tables created

---
+++ db/music_collection.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS artists;
+DROP TABLE IF EXISTS albums;
+
+
+CREATE TABLE artists(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+
+);
+
+CREATE TABLE albums(
+  id SERIAL8 PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  genre VARCHAR(255) NOT NULL,
+  artist_id INT2 REFERENCES artists(id)
+);
